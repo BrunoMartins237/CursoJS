9aa3435817ce9d93e64a0fe4fd5385ba82b09757
BrunoMartins237 <bcamilomenezes@gmail.com>
2021-09-27 19:19:11 -0300

    Rescrevendo o script

--- repetições/script.js
+++ repetições/script.js
@@ -4,29 +4,28 @@ function contador(){
     let passo = window.document.getElementById('passo')
     let res = window.document.getElementById('res')
     
-    if (inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0 ){
-        window.alert('Insira os dados corretamente!')
-    } else {
-        res.innerHTML = `Contando:`
+    if (inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0){
+        window.alert('[ERRO] Insira os Dados Corretamente')
+    } else{
+        res.innerHTML = `Contando: `
         let i = Number(inicio.value)
         let f = Number(fim.value)
         let p = Number(passo.value)
         if (p <= 0){
-            window.alert('Comando Inválido')
+            window.alert('[ERRO] iniciando passo 1')
             p = 1
         }
         if (i < f){
-        for (let c = i; c <= f; c += p){
-            res.innerHTML += ` ${c} \u{1F449}`
+            for (let c = i; c <= f; c += p){
+                res.innerHTML += `${c} \u{1F449} `
+            } res.innerHTML += ` \u{1F3C1}	`
+        } else{
+            for (let c = i; c >= f; c -= p){
+                res.innerHTML += `${c} \u{1F449}`
+            }res.innerHTML += ` \u{1F3C1}	`
         }
-        res.innerHTML += ` \u{1F3C1} `
-        } else {
-            for (let c = i ; c >= f; c -= p){
-                res.innerHTML += ` ${c} \u{1F449}`
-            } 
-            res.innerHTML += `\u{1F3C1}`
-        }
-    }   
+
+    }
 }
 
     
